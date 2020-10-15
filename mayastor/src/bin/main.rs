b3f40adda3e5b5682450bb833384522b193b43ec
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-10-15 19:57:41 +0200

    mainrs

--- mayastor/src/bin/main.rs
+++ mayastor/src/bin/main.rs
@@ -1,20 +1,27 @@
 #[macro_use]
 extern crate tracing;
 
-use std::path::Path;
-
-use structopt::StructOpt;
-
+use crossbeam::crossbeam_channel::bounded;
+use futures::FutureExt;
 use mayastor::{
     bdev::util::uring,
-    core::{MayastorCliArgs, MayastorEnvironment},
+    core::{MayastorCliArgs, MayastorEnvironment, Reactor, Reactors},
+    grpc,
     logger,
+    subsys,
 };
-
+use std::{net::SocketAddr, path::Path};
+use structopt::StructOpt;
+use tokio::prelude::*;
 mayastor::CPS_INIT!();
-
-fn main() -> Result<(), std::io::Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = MayastorCliArgs::from_args();
+
+    let mut rt = tokio::runtime::Builder::new()
+        .basic_scheduler()
+        .enable_all()
+        .build()
+        .unwrap();
 
     // setup our logger first if -L is passed, raise the log level
     // automatically. trace maps to debug at FFI level. If RUST_LOG is
@@ -44,10 +51,21 @@ fn main() -> Result<(), std::io::Error> {
         if uring_supported { "yes" } else { "no" }
     );
     info!("free_pages: {} nr_pages: {}", free_pages, nr_pages);
-    let env = MayastorEnvironment::new(args);
-    env.start(|| {
-        info!("Mayastor started {} ...", '\u{1F680}');
-    })
-    .unwrap();
+
+    let grpc_endpoint = grpc::endpoint(args.grpc_endpoint.clone());
+
+    let ms = rt.enter(|| MayastorEnvironment::new(args).init());
+
+    let master = Reactors::master();
+    let mut futures = Vec::new();
+
+    futures.push(master.boxed_local());
+    futures.push(subsys::Registration::run().boxed_local());
+    futures.push(grpc::MayastorGrpcServer::run(grpc_endpoint).boxed_local());
+
+    rt.block_on(futures::future::try_join_all(futures))
+        .expect_err("reactor exit in abnormal state");
+
+    ms.fini();
     Ok(())
 }
