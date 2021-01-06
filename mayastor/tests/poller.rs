ab62f2b91dbe481042733d9f0de7e19231d9c461
Jeffry Molanus <jeffry.molanus@gmail.com>
2021-01-06 16:15:22 +0100

    feat(poller): want poller abstraction
    This change adds a safe abstraction around the poller interface. We
    did not use pollers much (if not at all) but now that we need to
    construct, among others our own qpairs we need to create them often
    and frequently.
    
    A small trampoline function (which is always inlined) is used to avoid
    passing around raw pointers. The poller will execute a closure and allows
    us to capture variables so there is no need for boxing arguments.
    
    Implements CAS-610

---
+++ mayastor/tests/poller.rs
@@ -0,0 +1,81 @@
+use crossbeam::atomic::AtomicCell;
+use once_cell::sync::Lazy;
+
+use mayastor::core::{
+    mayastor_env_stop,
+    poller,
+    MayastorCliArgs,
+    MayastorEnvironment,
+    Reactors,
+};
+
+pub mod common;
+
+static COUNT: Lazy<AtomicCell<u32>> = Lazy::new(|| AtomicCell::new(0));
+
+fn test_fn(a: u32, b: u32) -> i32 {
+    let a = a + b;
+    println!("{}", a);
+    0
+}
+
+#[test]
+fn poller() {
+    common::mayastor_test_init();
+    MayastorEnvironment::new(MayastorCliArgs::default()).init();
+
+    let args = (1, 2);
+    let poller = poller::Builder::new()
+        .with_interval(0)
+        .with_poll_fn(move || {
+            println!("and a {} and {}", args.0, args.1);
+            let mut count = COUNT.load();
+            count += 1;
+            COUNT.store(count);
+            0
+        })
+        .build();
+
+    drop(poller);
+
+    Reactors::master().poll_once();
+
+    // we dropped the poller before we polled, the value should still be 0
+    assert_eq!(COUNT.load(), 0);
+
+    let args = (1, 2);
+    let mut poller = poller::Builder::new()
+        .with_interval(0)
+        .with_poll_fn(move || {
+            let count = COUNT.load();
+            println!("and a {} and {} (count: {}) ", args.0, args.1, count);
+            COUNT.store(count + 1);
+            0
+        })
+        .build();
+
+    Reactors::master().poll_times(64);
+    // we dropped the poller before we polled, it should not be there anymore.
+    assert_eq!(COUNT.load(), 64);
+
+    poller.pause();
+    Reactors::master().poll_times(64);
+    assert_eq!(COUNT.load(), 64);
+
+    poller.resume();
+    Reactors::master().poll_times(64);
+    assert_eq!(COUNT.load(), 128);
+
+    poller.stop();
+    Reactors::master().poll_times(64);
+    assert_eq!(COUNT.load(), 128);
+
+    let poller = poller::Builder::new()
+        .with_interval(0)
+        .with_poll_fn(|| test_fn(1, 2))
+        .build();
+    Reactors::master().poll_times(64);
+    drop(poller);
+
+    mayastor_env_stop(0);
+}
